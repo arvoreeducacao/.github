487d402cdc851006e61bfe59d7a6033ca762d5d6
João Vagner Brito de Medeiros <joaovagner@ntp1.tummy.com>
2022-08-26 10:05:31 -0300

    fixing bug to empty vars

--- setup-aws-py/main.py
+++ setup-aws-py/main.py
@@ -12,42 +12,35 @@ def main(aws_profile, root_path):
     else:
         print (f"Successfully created the directory {root_path}.aws")
 
-    ORG_TF_AWS_KEY_ID = os.environ["ORG_TF_AWS_KEY_ID"]
-    ORG_TF_AWS_SECRET_KEY = os.environ["ORG_TF_AWS_SECRET_KEY"]
-    
-    HML_TF_AWS_KEY_ID = os.environ["HML_TF_AWS_KEY_ID"]
-    HML_TF_AWS_SECRET_KEY = os.environ["HML_TF_AWS_SECRET_KEY"]
-    
-    PRD_TF_AWS_KEY_ID = os.environ["PRD_TF_AWS_KEY_ID"]
-    PRD_TF_AWS_SECRET_KEY = os.environ["PRD_TF_AWS_SECRET_KEY"]    
-
-    # DL_TF_AWS_KEY_ID = os.environ["DL_TF_AWS_KEY_ID"]
-    # DL_TF_AWS_SECRET_KEY = os.environ["DL_TF_AWS_SECRET_KEY"]    
 
     fileAws = open(f"{root_path}.aws/credentials","w")
 
-    fileAws.write("[org]\n")
-    fileAws.write(f"aws_access_key_id={ ORG_TF_AWS_KEY_ID }\n")
-    fileAws.write(f"aws_secret_access_key={ ORG_TF_AWS_SECRET_KEY }\n\n")
+    if os.environ["ORG_TF_AWS_KEY_ID"] != None :
+        ORG_TF_AWS_KEY_ID = os.environ["ORG_TF_AWS_KEY_ID"]
+        ORG_TF_AWS_SECRET_KEY = os.environ["ORG_TF_AWS_SECRET_KEY"]
+        
+        fileAws.write("[org]\n")
+        fileAws.write(f"aws_access_key_id={ ORG_TF_AWS_KEY_ID }\n")
+        fileAws.write(f"aws_secret_access_key={ ORG_TF_AWS_SECRET_KEY }\n\n")
 
-    fileAws.write("[dev]\n")
-    fileAws.write(f"aws_access_key_id={ HML_TF_AWS_KEY_ID }\n")
-    fileAws.write(f"aws_secret_access_key={ HML_TF_AWS_SECRET_KEY }\n\n")
+    if os.environ["HML_TF_AWS_KEY_ID"] != None :
 
-    fileAws.write("[hml]\n")
-    fileAws.write(f"aws_access_key_id={ HML_TF_AWS_KEY_ID }\n")
-    fileAws.write(f"aws_secret_access_key={ HML_TF_AWS_SECRET_KEY }\n\n")
+        HML_TF_AWS_KEY_ID = os.environ["HML_TF_AWS_KEY_ID"]
+        HML_TF_AWS_SECRET_KEY = os.environ["HML_TF_AWS_SECRET_KEY"]
+            
+        fileAws.write("[hml]\n")
+        fileAws.write(f"aws_access_key_id={ HML_TF_AWS_KEY_ID }\n")
+        fileAws.write(f"aws_secret_access_key={ HML_TF_AWS_SECRET_KEY }\n\n")
 
-    fileAws.write("[prd]\n")
-    fileAws.write(f"aws_access_key_id={ PRD_TF_AWS_KEY_ID }\n")
-    fileAws.write(f"aws_secret_access_key={ PRD_TF_AWS_SECRET_KEY }\n\n")
+    if os.environ["PRD_TF_AWS_KEY_ID"] != None :
+        PRD_TF_AWS_KEY_ID = os.environ["PRD_TF_AWS_KEY_ID"]
+        PRD_TF_AWS_SECRET_KEY = os.environ["PRD_TF_AWS_SECRET_KEY"]    
 
-    # fileAws.write("[dl]\n")
-    # fileAws.write(f"aws_access_key_id={ DL_TF_AWS_KEY_ID }\n")
-    # fileAws.write(f"aws_secret_access_key={ DL_TF_AWS_SECRET_KEY }\n")
+        fileAws.write("[prd]\n")
+        fileAws.write(f"aws_access_key_id={ PRD_TF_AWS_KEY_ID }\n")
+        fileAws.write(f"aws_secret_access_key={ PRD_TF_AWS_SECRET_KEY }\n\n")
+
     # fileAws.write(f"region=sa-east-1")
-
-
     fileAws.close()    
 
     os.environ["AWS_PROFILE"] = aws_profile.lower()
